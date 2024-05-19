5f4fc1fa347b7b471374261c764c76b512e9c1cc
milah1612 <umilah97@gmail.com>
2024-05-20 01:35:41 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,13 +2,11 @@
 # coding: utf-8
 
 # In[ ]:
-
 import streamlit as st
 from transformers import pipeline
 import pandas as pd
 from datetime import datetime, timedelta
 import pytz 
-pip install transformers
 
 # Load pretrained sentiment analysis pipeline
 sentiment_pipeline = pipeline("sentiment-analysis")

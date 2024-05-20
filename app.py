40e0ef51b6cbc3d5ad22d584d5a32d5c858b0ca1
milah1612 <umilah97@gmail.com>
2024-05-20 15:37:19 +0800

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,9 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import io 
 from langdetect import detect   
-from collections import Counter
+from collections import Counter 
+import plotly.express as px
+import helper_functions as hf  # Assuming your helper functions are in this module
 
 
 # Function to load CSS file
@@ -59,14 +61,25 @@ def is_english(text):
     return all(ord(char) < 128 for char in text) 
 
 
-# Initialize session state
+# Function to load the dataset
+@st.cache
+def load_data():
+    df = pd.read_csv('path_to_your_dataset.csv')  # Replace with your dataset path
+    return df
+
+# Load initial dataset
+if 'df' not in st.session_state or st.session_state.df.empty:
+    st.session_state.df = load_data()
+    st.session_state.df['Processed Text'] = st.session_state.df['tweet_text'].apply(preprocess_text)
+    st.session_state.df['Sentiment'] = st.session_state.df['tweet_text'].apply(predict_sentiment)
+
+# Initialize session state for tweets
 if 'tweets' not in st.session_state:
     st.session_state.tweets = []  
     
-
 # Streamlit app
 # Sidebar configuration
-st.sidebar.image("twitter_icon.png", width=200, output_format='png', use_column_width=False)  # Replace "twitter_icon.png" with the actual filename and path  
+st.sidebar.image("twitter_icon.png", width=200, output_format='png', use_column_width=False)  # Replace with actual filename and path  
 st.sidebar.title("TWITTER SENTIMENT ANALYSIS")
 st.sidebar.write("This application performs sentiment analysis on the latest tweets based on the entered search term. The application can only predict positive or negative sentiment, and only English tweets are supported.")
 
@@ -77,7 +90,6 @@ user_input = st.sidebar.text_area("Enter the search term or tweet for sentiment 
 if st.sidebar.button("Analyze Sentiment"):
     if user_input:
         try:
-            # Perform language detection
             detected_language = detect(user_input)
             if detected_language != 'en':  # Check if the detected language is not English
                 st.sidebar.write("Please enter text in English.")
@@ -85,11 +97,13 @@ if st.sidebar.button("Analyze Sentiment"):
                 prediction = predict_sentiment(user_input)
                 processed_text = preprocess_text(user_input)
 
+                # Add new tweet to the dataset
+                new_tweet = {'tweet_text': user_input, 'cyberbullying_type': 'unknown', 'Processed Text': processed_text, 'Sentiment': prediction}
+                st.session_state.df = st.session_state.df.append(new_tweet, ignore_index=True)
+
                 # Display the prediction result in the sidebar
                 st.sidebar.subheader("Analysis Result")
                 st.sidebar.write(f"Processed Text: {processed_text}")
-
-                # Check the prediction and handle it
                 if prediction == 1:
                     st.sidebar.write("Sentiment: Positive")
                 elif prediction == 0:
@@ -101,18 +115,19 @@ if st.sidebar.button("Analyze Sentiment"):
     else:
         st.sidebar.write("Please enter some text.") 
 
-
 # Function to make the dashboard
-def make_dashboard(tweet_df, bar_color, wc_color):
-    # Make 3 columns for the first row of the dashboard
-    col1, col2, col3 = st.columns([28, 34, 38])
+def make_dashboard(tweet_df, bar_color):
+    # Make 2 columns for the first row of the dashboard
+    col1, col2 = st.columns([50, 50])
     with col1:
         # Plot the sentiment distribution
         sentiment_plot = hf.plot_sentiment(tweet_df)
         sentiment_plot.update_layout(height=350, title_x=0.5)
         st.plotly_chart(sentiment_plot, theme=None, use_container_width=True)
 
-    with col2:
+    # Make 2 columns for the second row of the dashboard
+    col1, col2 = st.columns([50, 50])
+    with col1:
         # Plot the top 10 occurring words 
         top_unigram = hf.get_top_n_gram(tweet_df, ngram_range=(1, 1), n=10)
         unigram_plot = hf.plot_n_gram(
@@ -121,7 +136,7 @@ def make_dashboard(tweet_df, bar_color, wc_color):
         unigram_plot.update_layout(height=350)
         st.plotly_chart(unigram_plot, theme=None, use_container_width=True)
 
-    with col3:
+    with col2:
         # Plot the top 10 occurring bigrams
         top_bigram = hf.get_top_n_gram(tweet_df, ngram_range=(2, 2), n=10)
         bigram_plot = hf.plot_n_gram(
@@ -129,29 +144,6 @@ def make_dashboard(tweet_df, bar_color, wc_color):
         )
         bigram_plot.update_layout(height=350)
         st.plotly_chart(bigram_plot, theme=None, use_container_width=True)
-
-    # Make 2 columns for the second row of the dashboard
-    col1, col2 = st.columns([60, 40])
-    with col1:
-        # Function to color the sentiment column
-        def sentiment_color(sentiment):
-            if sentiment == "Positive":
-                return "background-color: #1F77B4; color: white"
-            else:
-                return "background-color: #FF7F0E"
-
-        # Show the dataframe containing the tweets and their sentiment
-        st.dataframe(
-            tweet_df[["Sentiment", "Tweet"]].style.applymap(
-                sentiment_color, subset=["Sentiment"]
-            ),
-            height=350
-        )
-
-    with col2:
-        # Plot the wordcloud
-        wordcloud = hf.plot_wordcloud(tweet_df, colormap=wc_color)
-        st.pyplot(wordcloud)
 
 # Increase the font size of text inside the tabs
 adjust_tab_font = """
@@ -174,25 +166,24 @@ selected_tab = st.radio("Select sentiment:", ["All", "Positive 😊", "Negative 
 
 # Display content based on the selected tab
 if selected_tab == "All":
-    # Make dashboard for all tweets
-    if "df" in st.session_state:
-        tweet_df = st.session_state.df
-        make_dashboard(tweet_df, bar_color="#54A24B", wc_color="Greens")
+    # Show the table with sentiment and tweet column
+    if not st.session_state.df.empty:
+        st.dataframe(st.session_state.df[['Sentiment', 'tweet_text']])
     else:
         st.write("No tweets to display.")
 
 elif selected_tab == "Positive 😊":
     # Make dashboard for tweets with positive sentiment
-    if "df" in st.session_state:
-        tweet_df = st.session_state.df.query("Sentiment == 'Positive'")
-        make_dashboard(tweet_df, bar_color="#1F77B4", wc_color="Blues")
+    if not st.session_state.df.empty:
+        tweet_df = st.session_state.df.query("Sentiment == 1")
+        make_dashboard(tweet_df, bar_color="#1F77B4")
     else:
         st.write("No positive tweets to display.")
 
 else:
     # Make dashboard for tweets with negative sentiment
-    if "df" in st.session_state:
-        tweet_df = st.session_state.df.query("Sentiment == 'Negative'")
-        make_dashboard(tweet_df, bar_color="#FF7F0E", wc_color="Oranges")
+    if not st.session_state.df.empty:
+        tweet_df = st.session_state.df.query("Sentiment == 0")
+        make_dashboard(tweet_df, bar_color="#FF7F0E")
     else:
         st.write("No negative tweets to display.")

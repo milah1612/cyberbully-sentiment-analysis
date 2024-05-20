d5d634aea613c981df09836f4f55ce2d5c30be11
milah1612 <umilah97@gmail.com>
2024-05-21 03:53:50 +0800

    Update app.py

--- app.py
+++ app.py
@@ -63,10 +63,6 @@ if 'df' not in st.session_state:
 # URL of the CSV file hosted on GitHub
 csv_url = 'https://raw.githubusercontent.com/milah1612/cyberbully-sentiment-analysis/main/tweets.csv' 
 
-# Load initial dataset into session state
-if st.session_state.df.empty:
-    st.session_state.df = load_data(url)
-
 # Function to load data from a URL
 @st.cache(allow_output_mutation=True)
 def load_data(url):
@@ -83,7 +79,12 @@ def load_data(url):
         return pd.DataFrame()  # Return empty DataFrame in case of error  
     except Exception as ex:
         st.error(f"An unexpected error occurred: {ex}")
-        return pd.DataFrame()  # Return empty DataFrame for any other exceptions
+        return pd.DataFrame()  # Return empty DataFrame for any other exceptions 
+
+
+# Load initial dataset into session state
+if st.session_state.df.empty:
+    st.session_state.df = load_data(csv_url)
 
 
 # Function to add new tweet and update the dataset

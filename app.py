49e5e41c5fafb3ccd9577dc2d56ec5259c286c34
milah1612 <umilah97@gmail.com>
2024-05-20 16:44:38 +0800

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,8 @@ import io
 from langdetect import detect   
 from collections import Counter 
 import plotly.express as px 
-from hf import plot_sentiment
+from hf import plot_sentiment, get_top_n_gram  
+
 
 # Function to load CSS file
 def local_css(file_path):
